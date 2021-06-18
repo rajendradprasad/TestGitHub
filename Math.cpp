f274d4a7ea5086688e204ea5da2e22e8fd1e19e7
rajendradprasad <rajendradprasad@gmail.com>
2021-06-18 21:47:17 +0530

    Adding subtract code

--- Math.cpp
+++ Math.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 
+int Subtract(int a, int b)
+{
+  return a-b;
+}
+
 int Add(int a, int b)
 {
   return a+b;
@@ -9,4 +14,5 @@ void main()
 {
   std::cout << "Math functions\n";
   std::cout << "Add(4,5): " << Add(4,5); 
+  std::cout << "Subtract(4,5): " << Add(4,5); 
 }
